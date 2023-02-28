0557868856b928e6ceb6aae55ab6719d1a0bfa26
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-02-28 22:47:47 +0530

    Add files via upload

---
+++ enumerates.py
@@ -0,0 +1,14 @@
+List1= [2,23,43,54,56,66,67]
+for index,item in enumerate(List1):
+  print(item,index)
+  
+  
+  # simpley prints the item with indext number
+  
+l = [1,2,3,4,5,6,7,8,8]
+
+for index,item in enumerate(l):
+  print(item,index)
+     
+  
+  
