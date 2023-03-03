0d7ee57b82e1d1e1d1f9a890726bc3ae876c231f
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-03 22:03:55 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,16 @@
+#import turtle 
+#import time
+#def fun():
+#    colors = ["red","blue","Green","Yellow","brown","orange"]
+#    t = turtle
+#    t.pensize(5)
+#    t.bgcolor('black')
+#    t.speed(1000)
+#    for x in range(360):
+#        t.pencolor(colors[x%len(colors)])
+#        t.pensize(x/50)
+#        t.forward(x)
+#        t.left(59)
+#fun()
+#time.sleep(5)
+#
