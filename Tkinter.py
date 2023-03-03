96d91a5766e71c143700289eab6c3e88e7de64e9
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-03 22:04:04 +0530

    Add files via upload

---
+++ Tkinter.py
@@ -0,0 +1,20 @@
+#1. import tkinter --- 2. create GUI application main window
+#4. enter main event loop --- 3. add widget
+
+import Tkinter
+window = Tkinter.TK()
+window.title("GUI")
+label =Tkinter.label(window,text = "hello").pack()
+window.mainloop()
+
+import Tkinter
+window = Tkinter.TK()
+window.title('GGUI')
+label = Tkinter
+
+import Tkinter
+window = Tkinter.TK()
+window.title('gff')
+label = ltkinter.label(window,text = )pack()
+
+ 
