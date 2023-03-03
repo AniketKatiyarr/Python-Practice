f584ed864965a8cb9d5d6c44c8d3e17e1cadea9a
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-03 22:04:29 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,13 @@
+name= "Aniketkatiyar"
+print(name[2])
+print(name[0:6])
+print(name[:4]) #is same aas name {0:4}
+
+
+a = "aniketkatiyar"
+print(a[-2 :-4])
+print(a[1:9:2])
+print(a[-3:-8])
+     
+ 
+
