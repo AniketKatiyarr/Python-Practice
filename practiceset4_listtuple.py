99bebfc05a2e65e29ff7a6276b6bb0a21cb7b0f9
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-04 23:33:21 +0530

    Add files via upload

---
+++ practiceset4_listtuple.py
@@ -0,0 +1,31 @@
+#1,, lemon
+
+'''a1 = input("enter fruit no 1")
+a2 = input("enter fruit no 2")
+a3 = input("enter fruit no 3")
+a4 = input("enter fruit no 4")
+a5 = input("enter fruit no 5")
+a6 = input("enter fruit no 6")
+a7 = input("enter fruit no 7")
+
+myfruit = [a1,a2,a3,a4,a5,a6,a7]
+print(myfruit) '''
+
+#2.
+
+a1 = input("enter marks no 1")
+a2 = input("enter marks no 2")
+a3 = input("enter marks no 3")
+a4 = input("enter marks no 4")
+a5 = input("enter marks no 5")
+a6 = input("enter marks no 6")
+a7 = input("enter marks no 7")
+
+marks = [a1,a2,a3,a4,a5,a6,a7]
+
+marks.sort()
+print(marks)
+
+
+
+  
