d334c406759ba5ec14ca847346e1ae5a437c4389
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-04 23:32:17 +0530

    Add files via upload

---
+++ stringFunction.py
@@ -0,0 +1,32 @@
+#function  of strign length
+print(len("Aniket"))
+name = "Aniketkatiyar"
+print(len(name))
+
+#endswith funtion
+
+print(name.endswith("r"))
+print(name.endswith("yar"))
+
+# count funtion 
+print(name.count("a"))
+print(name.count("i"))
+
+#capitalize funtion 
+print(name.capitalize())
+print(name.capitalize())
+
+#function find
+print(name.find("Aniketkatiyar"))   
+
+#function replace
+print(name.replace("Aniketkatiyar" , "katiyarAniket"))
+
+#escape sequence character
+
+# \n new line
+# \t tab space
+# \single quote
+# \\ backslash
+
+
