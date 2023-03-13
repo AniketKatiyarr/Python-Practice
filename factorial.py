d04021dd39f4f0b80e022fd6789792906b3d98ef
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-13 22:39:18 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+numm = int(input("enter your number"))
+factorial = 1
+for i in range(1, numm+1):
+    factorial = factorial * 1
+    print(f"the factorial of given number is{factorial}")
+  
+
+n = 4
+for i in range(4):
+    print("*",*(i+1))
+    
+
