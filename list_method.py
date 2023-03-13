4d2b9a16c56f3a4966769e4048ac208e97fed4c8
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-13 22:37:51 +0530

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,29 @@
+#sort method 
+L1 = [1,8,7,2,12,43]
+print(L1)
+L1.sort()
+print(L1)
+
+#reverse method 
+L1.reverse()
+print(L1)
+
+#append method 
+#it is use to add a integer at the end of lists
+L1.append(23)
+print(L1)
+L1.append(34)
+print(L1)
+
+#insert method
+
+L1.insert(2,10)
+print(L1)
+
+#pop method used to delete integer at index
+L1.pop(2)
+print(L1)
+
+#remove method is used to remove 15 from list
+L1.remove(43)
+print(L1)
