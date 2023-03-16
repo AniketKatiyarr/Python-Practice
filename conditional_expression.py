7d6b9a7fbc1a5bad253d28734466924dfa28a37a
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-16 23:19:17 +0530

    Add files via upload

---
+++ conditional_expression.py
@@ -0,0 +1,35 @@
+
+a = 35
+if(a>40):
+    print("the value is greater than 45")
+elif(a>10):
+    print("the value is greeater then 10")
+else:
+    print("none value is greater")        
+
+#2 if two if are use than this happen
+a = 50
+if(a>40):
+    print("the value is greater than 45")
+if(a>42):
+    print("the value is not")
+
+if(a>10):
+    print("the value is greeater then 10")
+else:
+    print("none value is greater")               
+print("done") 
+   
+#3 print yes age greater than 18
+a = int(input("enter the age:"))
+if(a>18):
+   print("you are eiligible")
+
+else:
+   print("you are ot eligible")
+   
+   print("done")   
+
+ 
+ 
+             
