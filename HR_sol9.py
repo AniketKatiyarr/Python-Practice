6032072c5bba20b8eee87e35e0ed481d8129b14f
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-18 21:46:11 +0530

    Add files via upload

---
+++ HR_sol9.py
@@ -0,0 +1,19 @@
+'''Sample Input
+
+5
+Sample Output
+
+1
+22
+333
+4444'''
+
+#a = int(input())
+#for i in range(1,a):
+#    for j in range(1,i+1):
+#      print(i,end="")
+
+
+#''' next level challenge '''
+for i in range(1,int(input())):
+    print(pow(10,i)//9*i)
